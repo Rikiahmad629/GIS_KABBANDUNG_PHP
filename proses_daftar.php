fd2d35797a97ca69e0c3f876a420d76264dd8c2b
fazhal <fazhaldarul@gmail.com>
2015-06-02 19:25:32 +0700

    proses daftar

--- proses_daftar.php
+++ proses_daftar.php
@@ -6,14 +6,39 @@ if($_FILES['fotoktp']['error']==0){
 	$info_path = pathinfo($_FILES['fotoktp']['name']);
 	 $extension = $info_path['extension'];
  	//ganti nama pake waktu sekarang
- 	$namafilebaru="images/".time().".".$extension;
+	 $time = time();
+ 	 $namafilebaru="images/ktp/".$time.".".$extension;
 
 	$is_in_array = in_array($extension,$file_type,true);
  	if($is_in_array){
+ 		$nama = $_POST['nama'];
+ 		$no_ktp = $_POST['no_ktp'];
+ 		$tempat_lahir = $_POST['tempat_lahir'];
+ 		$tanggal_lahir = $_POST['tanggal_lahir'];
+ 		$email = $_POST['email'];
+ 		$password = $_POST['password'];
+ 		$alamat = $_POST['alamat'];
+ 		$phone = $_POST['no_hp'];
+ 		
  		if(move_uploaded_file($_FILES['fotoktp']['tmp_name'],$namafilebaru)==true){
-		 	?>
-		 	<script>alert("Sukses Daftar");window.location="index.php"</script>
-		 	<?php
+ 			$namafilebaru = $time.".".$extension;
+ 			$query1 = "select * from users where no_ktp = '".$no_ktp."'";
+ 			$data = mysql_query($query1);
+ 			$jumlah = mysql_num_rows($data);
+ 			if($jumlah > 0){
+ 				?>
+			 	 <script type="text/javascript">alert("Gagal No KTP sudah ada");history.back();</script>>
+			 	<?php	
+ 			}else{
+ 				$query = "insert into users  (nama,no_ktp,tempat_lahir,tanggal_lahir,email,password,alamat,phone,image_ktp) 
+					  values('".$nama."','".$no_ktp."','".$tempat_lahir."','".$tanggal_lahir."','".$email."',
+					  '".$password."','".$alamat."','".$phone."','".$namafilebaru."')";
+				mysql_query($query);
+			 	?>
+			 	 <script type="text/javascript">alert("Sukses Daftar");window.location="index.php"</script>>
+			 	<?php	
+ 			}
+ 			
  		}
 	}else{		
 		echo "FILE TIDAK DIIJINKAN!!";
