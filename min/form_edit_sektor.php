8c72fc4dde9f03acf3528147eca3d24e8d58633f
TianSeptian <tian81093@gmail.com>
2015-06-08 23:26:51 +0700

    Create form_edit_sektor.php

---
+++ min/form_edit_sektor.php
@@ -0,0 +1,54 @@
+ <?php include("../template/header_admin.php");?>
+
+  <body>
+ <style>
+      #map-canvas {
+        height: 200px;
+      
+      }
+      #message{
+        color:red;
+      }
+    </style>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+       <?php include("../template/menu_atas_admin.php");?>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <?php include("../template/menu_samping_admin.php");?>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h2>Form Edit Sektor</h2>
+            <?php
+			$id=$_GET['id'];
+                $query = "select * from sektor where id_sektor='$id'";
+                $q = mysql_query($query) or die(mysql_error());
+                $data = mysql_fetch_array($q);
+             ?>
+        <form class="form-horizontal" method="POST" action="proses_edit_sektor.php"  enctype="multipart/form-data">
+        <div class="form-group">
+          <label for="edit_sektor" class="col-sm-2 control-label">ID Sektor</label>
+          <div class="col-sm-10">
+            <input type="text" name="id_sektor" value="<?php echo $data['id_sektor'];?>" readonly>
+          </div>
+        </div>
+         
+         <div class="form-group">
+          <label for="nama" class="col-sm-2 control-label">Nama</label>
+          <div class="col-sm-10">
+           <textarea class="form-control" rows="3" id="nama" name="nama" required><?php echo $data['nama_sektor'];?></textarea>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Update</button>
+          </div>
+        </div>
+      </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
